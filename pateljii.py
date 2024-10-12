73fa8deda4a856a17d772288dfd9c9805b9223ef
shivam071993 <shivamingale627@gmail.com>
2024-10-12 23:45:08 +0530

    Add files via upload

---
+++ pateljii.py
@@ -0,0 +1,8 @@
+# Bot token and admin ID
+TOKEN = "7915229015:AAFPv6esbnwmxhyLFDf0-2oaeDCrTm3gnXw"  # Bot Token
+ADMIN_ID = 7431484129  # Admin ID
+
+# Messages
+APPROVED_MESSAGE = "❤️Aap approved hain. Swagat hai 😊 DM--> @patelji_47 !"
+PENDING_APPROVAL_MESSAGE = "Aapka approval pending hai. Kripya admin ke approve hone ka intezaar karein DM--> @patelji_47 \nUser ID ko provide karein. Example: /approve <user_id>."
+ATTACK_BUTTON_MESSAGE = "By  Please enter the target, port, and time in the format:<target> <port> 🚀🚀\n\nAttack shuru karne ke liye button dabaayein. DM--> @patelji_47 "
